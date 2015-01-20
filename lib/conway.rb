56ae292d706ab1bd922bc4dcdbc4ed1a208fb9c5
katyf <katy.feng.14@gmail.com>
2015-01-20 16:33:34 -0500

    silent coder

---
+++ lib/conway.rb
@@ -0,0 +1,13 @@
+class Cell
+  def initialize(status, x_pos, y_pos)
+    @status = status
+    @x_pos = x_pos
+    @y_pos = y_pos
+
+  end
+
+  # def is_alive?
+  #   # @x_pos.do |item, index|
+  #   if
+  # end
+end
--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,1 +1,32 @@
 require_relative '../lib/conway'
+
+describe Cell do
+  let(:status) {:alive}
+  let(:x_pos) {[0]}
+  let(:y_pos) {[0]}
+#we need more cells
+  subject(:cell_1) {Cell.new(status, x_pos, y_pos)}
+  subject(:cell_2) {Cell.new(status, x_pos, y_pos)}
+  subject(:cell_3) {Cell.new(status, x_pos, y_pos)}
+  subject(:cell_4) {Cell.new(status, x_pos, y_pos)}
+
+
+  describe "#initialize" do
+    it "creates a new cell" do
+      expect(cell_1).to be_a Cell
+      expect(cell_2).to be_a Cell
+      expect(cell_3).to be_a Cell
+      expect(cell_4).to be_a Cell
+    end
+  end
+
+  describe "#alive?" do
+    it "returns if a cell is alive" do
+      expect(cell_1).to eq :alive
+    end
+  end
+
+
+
+
+end
